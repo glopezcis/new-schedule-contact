f9deed6ab662ff44732ab65ad74789bbd9d69be1
glopezcis <glopezcis@gmail.com>
2021-03-24 12:46:57 -0500

    Added dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NewScheduleContact from './components/NewScheduleContact';
 
 ReactDOM.render(
   <React.StrictMode>
