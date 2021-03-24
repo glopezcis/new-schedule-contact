d5c641930492c4952780bb45ec7aca7970bcdc03
glopezcis <glopezcis@gmail.com>
2021-03-24 13:28:25 -0500

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import NewScheduleContact from './components/NewScheduleContact';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
