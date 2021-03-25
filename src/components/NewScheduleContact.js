760bd9906f5d063317335f48f316c18d9d38b794
glopezcis <glopezcis@gmail.com>
2021-03-25 09:54:10 -0500

    No comments

--- src/components/NewScheduleContact.js
+++ src/components/NewScheduleContact.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const NewScheduleContact = <h1>texto</h1>
+const NewScheduleContact = () => {
+	return (
+		<div>
+			<h1>texto</h1>
+		</div>
+	);
+};
 
 export default NewScheduleContact;
