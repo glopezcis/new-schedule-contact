50b0739b2c8c043a344a4f2690bb7e1e01e814fb
glopezcis <glopezcis@gmail.com>
2021-03-24 20:21:29 -0500

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    react: "react"
+  }
+};
