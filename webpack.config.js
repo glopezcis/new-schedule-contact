8e27b1929c79576e4eacc9e89caf2e90123e98c7
glopezcis <glopezcis@gmail.com>
2021-03-25 10:57:35 -0500

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,10 @@ module.exports = {
             presets: [
               '@babel/preset-env', '@babel/react',
               {
-                'plugins': ['@babel/plugin-proposal-class-properties']
+                'plugins': [
+                  '@babel/plugin-proposal-class-properties',
+                  '@babel/plugin-syntax-jsx'
+                ]
               }
             ]
           }
